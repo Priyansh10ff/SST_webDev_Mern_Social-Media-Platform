ddfdaae9928d374a650bf5e370d12f8a5eee201a
Priyansh Dugar <priyansh10jan@gmail.com>
2026-08-31 23:12:40 +0530

    push

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import userRoutes from "./routes/user.routes.js";
+
 dotenv.config();
+const app = express();
 
 const port = 8001;
 
-const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    //methods
-    // allowedHeaders : ['Content-Type' : 'Authorization']
-  }),
-);
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/users", userRoutes);
-
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.dbURL)
   .then(() => {
-    console.log("Db Connected");
+    console.log("DB Connected");
   })
   .catch((err) => {
     console.log(err);
   });
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Must match exact frontend URL
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/users", userRoutes);
+
 app.listen(port, () => {
-  console.log("Server started");
+  console.log(`Server Started at ${port}`);
 });
