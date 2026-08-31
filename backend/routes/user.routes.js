196427913e490bf85534f11c713795d661297093
Priyansh Dugar <priyansh10jan@gmail.com>
2026-08-31 10:17:29 +0530

    fix: format user.routes.js for consistency and readability

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,15 @@
-import express from "express"
-import { registerUser, loginUser, getUser } from "../controllers/user.controllers.js"
-import isAuthenticated from "../middlewares/authMiddleware.js"
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getUser,
+} from "../controllers/user.controllers.js";
+import isAuthenticated from "../middlewares/authMiddleware.js";
 
-const userRoutes = express.Router()
+const userRoutes = express.Router();
 
-userRoutes.post('/register', registerUser)
-userRoutes.post('/login', loginUser)
-userRoutes.get('/me', isAuthenticated, getUser)
+userRoutes.post("/register", registerUser);
+userRoutes.post("/login", loginUser);
+userRoutes.get("/me", isAuthenticated, getUser);
 
-export default userRoutes
+export default userRoutes;
